68252023787cfd4d35019bed014e1e88b79f6432
Andreas Jim-Hartmann <andreas.hartmann@becompany.ch>
2017-05-04 12:54:28 +0200

    Remove old todomvc files.

--- src/main/scala/example/AppCircuit.scala
+++ src/main/scala/example/AppCircuit.scala
@@ -16,29 +16,3 @@ object AppCircuit extends Circuit[NetstatModel] with ReactConnector[NetstatModel
   )
 }
 
-class TodoHandler[M](modelRW: ModelRW[M, Seq[Todo]]) extends ActionHandler(modelRW) {
-
-  def updateOne(Id: TodoId)(f: Todo => Todo): Seq[Todo] =
-    value.map {
-      case found @ Todo(Id, _, _) => f(found)
-      case other                  => other
-    }
-
-  override def handle = {
-    case InitTodos =>
-      println("Initializing todos")
-      updated(List(Todo(TodoId.random, "Test your code!", false)))
-    case AddTodo(title) =>
-      updated(value :+ Todo(TodoId.random, title, false))
-    case ToggleAll(checked) =>
-      updated(value.map(_.copy(isCompleted = checked)))
-    case ToggleCompleted(id) =>
-      updated(updateOne(id)(old => old.copy(isCompleted = !old.isCompleted)))
-    case Update(id, title) =>
-      updated(updateOne(id)(_.copy(title = title)))
-    case Delete(id) =>
-      updated(value.filterNot(_.id == id))
-    case ClearCompleted =>
-      updated(value.filterNot(_.isCompleted))
-  }
-}
